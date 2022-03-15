be703f1b6d034fa663515df205f8b1748404b848
Antoine Millet <antoine@inaps.org>
2022-03-15 14:41:54 +0100

    qcow-provisioner: removed abandoned helm chart

--- charts/qcow-provisioner/init-node.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-rmmod nbd || echo "WARNING: failed to unload ndb module, could not set nbds_max"
-modprobe nbd max_part=16 nbds_max=$1
-sleep 10
-rm -f "$2/plugins/qcow.csi.enix.io/csi.sock"
-
-node -node-name "$QCOW_PROVISIONER_NODE_NAME" ${@:3}
